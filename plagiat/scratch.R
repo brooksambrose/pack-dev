cddb45fee6787d70b61d28e6969349e78270e211
Brooks Ambrose <brooksambrose@gmail.com>
2018-07-25 16:15:29 +0000

    jpdf2imp first draft

--- plagiat/scratch.R
+++ plagiat/scratch.R
@@ -443,7 +443,7 @@ long[,lang:=list(lapply(title,cld2::detect_language_mixed))]
 long[,`:=`(
   bg=sapply(lang,function(x) x[[1]][1,1])
   ,pg=sapply(lang,function(x) x[[1]][1,4])
-  )]
+)]
 setorder(long,bg,-pg,start)
 View(long)
 
@@ -458,13 +458,16 @@ View(long)
 #' @export
 #'
 #' @examples
-jpdf2imp<-function(jpdf,depth=5){
+jpdf2imp<-function(jpdf,ocr=F,depth=5){
   require(data.table)
   require(stringr)
   require(pdftools)
   require(stringdist)
   require(igraph)
   require(progress)
+  # require(parallel)
+  # ncores<-c(1,detectCores()/2 %>% ceiling) %>% max
+
   # import
   imp<-jpdf %>% lapply(pdf_text)
   imp %<>% lapply(str_split,'\n')
@@ -472,13 +475,32 @@ jpdf2imp<-function(jpdf,depth=5){
   cp<-lapply(imp,function(x) x[[1]])
   for(i in 1:length(imp)) imp[[i]][1]<-NULL
   # ocr
-  browser()
-  oc<-sapply(imp,function(x) x %>% unlist %>% nchar %>% sum)/sapply(imp,length)
-  oc<-which(oc<200)
-  if(!length(oc)){
-    require(tesseract)
-    for(i in oc){
-      pdf_info(jpdf[i])
+  oc<-sapply(imp,sapply,length) %>% sapply(function(x) 3%in%x) %>% which
+  if(length(oc)){
+    cat('\n',length(oc),' undigitized documents detected.',sep='')
+    if(!ocr) {
+      cat(' Removing from corpus:',basename(jpdf[oc]),sep='\n')
+      dropped<-jpdf[oc]
+      jpdf<-jpdf[-oc]
+      cp<-cp[-oc]
+      imp<-imp[-oc]
+    } else {
+      cat(' Performing optical character recognition:')
+      ol<-sapply(jpdf[oc],function(x) suppressMessages(pdftools::pdf_info(x))$pages)
+      pb <- progress_bar$new(format = "  [:bar] :elapsed :eta",total = sum(ol)-length(ol), clear = FALSE, width= 60)
+      ol<-sapply(ol,seq,from=2)
+      pb$tick(0)
+      for(i in 1:length(ol)) for(j in 1:length(ol[[i]])){
+        x<-pdftools::pdf_convert(
+          pdf = names(ol)[i]
+          ,dpi=300
+          ,pages=ol[[i]][j]
+          ,filenames = sprintf('%s/%s%03d.png',tempdir(),sub('.pdf','',basename(names(ol[i]))),ol[[i]][j])
+          ,verbose = F
+        )
+        imp[[oc[i]]][[j]]<-tesseract::ocr(x) %>% gsub('\n\n+','\n     ',.) %>% stringr::str_split('\n') %>% `[[`(1)
+        pb$tick()
+      }
     }
   }
   # metadata key
@@ -488,60 +510,74 @@ jpdf2imp<-function(jpdf,depth=5){
       if(!length(b)) b<-grep('^Source',x)
       x[1:(b-1)] %>% paste(collapse=' ') %>% sub(' +',' ',.)
     })
-    ,author=list(lapply(cp,function(x) {
+    ,author=lapply(cp,function(x) {
       b<-grep('^Author',x)
       e<-grep('^Source',x)
+      if((!length(b))|(!length(e))) return('')
       x[b:(e-1)] %>% paste(collapse=' ') %>% sub('^[^:]+: ','',.) %>% str_split(',|( and )') %>% unlist %>%
         sub('^ *','',.) %>% sub(' *$','',.) %>% sub(' +',' ',.) %>% `[`(.!='')
-    }))
-    ,source=list(lapply(cp,function(x) {
+    })
+    ,source=lapply(cp,function(x) {
       b<-grep('^Source',x)
       e<-grep('^Published',x)
       x[b:(e-1)] %>% paste(collapse=' ') %>% sub('^[^:]+: ','',.) %>% str_split('[,)(]') %>% unlist %>%
         sub('^ *','',.) %>% sub(' *$','',.) %>% sub(' +',' ',.) %>% `[`(.!='')
-    }))
-    ,url=list(lapply(cp,function(x) {
+    })
+    ,url=sapply(cp,function(x) {
       b<-grep('^Stable',x)
       x[b] %>% sub('^[^:]+: ','',.) %>% sub('^ *','',.) %>% sub(' *$','',.)
-    }))
-    ,accessed=list(lapply(cp,function(x) {
+    })
+    ,accessed=sapply(cp,function(x) {
       b<-grep('^Accessed',x)
       x[b] %>% sub('^[^:]+: ','',.) %>% sub('^ *','',.) %>% sub(' *$','',.)
-    }))
-    )
+    })
+  )
   meta[,doc:=jpdf]
   meta[,`:=`(
     journal=sapply(source,`[`,1)
     ,year=sapply(source,function(x) grep('^[0-9]{4}$',x,value = T) %>% tail(1))
-    )]
-  setcolorder(meta,c('doc','journal','year','title','author','source','url','accessed','doc'))
+  )]
+  setcolorder(meta,c('doc','journal','year','title','author','source','url','accessed'))
   # header detection, accurate solution is too slow
-  pb <- progress_bar$new(format = "  [:bar] :elapsed :eta",total = i*depth*2, clear = FALSE, width= 60)
+  cat('\nHeader detection\n')
   clusth<-function(y) {
     y<-gsub(' +',' ',y)
     m<-1-stringdistmatrix(y,y,method = 'jw',p = .1)
     n<-graph_from_adjacency_matrix(m,'undirected',weighted=T)
-    c<-cluster_spinglass(n) %>% communities
+    if(is_connected(n)) {c<-cluster_spinglass(n) %>% communities}else{c<-cluster_walktrap(n) %>% communities}
     r<-data.table(c=c)
     r[,s:=sapply(c,function(x) E(induced_subgraph(n,x))$weight%>% mean)]
+    r<-r[sapply(c,length)>1]
     pb$tick()
     r
   }
-  h<-lapply(imp,function(x) sapply(x,head,depth) %>% t %>% data.table %>% setnames(paste0('h',1:depth)))
-  hc<-lapply(h,function(x) lapply(x,clusth))
-  f<-lapply(imp,function(x) sapply(x,tail,depth) %>% t %>% data.table %>% setnames(paste0('f',1:depth)))
-  fc<-lapply(f,function(x) lapply(x,clusth))
-  for(i in 1:length(imp)){
+
+  h<-lapply(imp,function(x) {
+    d<-c(depth,sapply(x,length)) %>% min
+    sapply(x,function(y) {head(y,d)}) %>% t %>% data.table %>% setnames(paste0('h',1:ncol(.)))})
+  f<-lapply(imp,function(x) {
+    d<-c(depth,sapply(x,length)) %>% min
+    sapply(x,function(y) {tail(y,d)}) %>% t %>% data.table %>% setnames(paste0('f',1:ncol(.)))})
+
+  il<-sapply(imp,length)==1
+  pb <- progress_bar$new(format = "  [:bar] :elapsed :eta",total = sum(sapply(h,dim)[2,],sapply(f,dim)[2,]), clear = FALSE, width= 60)
+  pb$tick(0)
+  hc<-lapply(h[!il],function(x) lapply(x,clusth))
+  fc<-lapply(f[!il],function(x) lapply(x,clusth))
+  for(i in (1:length(imp))[!il]){
     l<-sapply(imp[[i]],length)
     drop<-sapply(hc[[i]],function(x) x[,mean(s)>.9&.N<5]) %>% which
     for(j in 1:length(l)) imp[[i]][[j]]<-imp[[i]][[j]][-c(drop,tail(1:l[j],depth)[sapply(fc[[i]],function(x) x[,mean(s)>.9&.N<5]) %>% which])]
   }
   # paragraph detection
+  cat('\nParagraph detection\n')
   imp<-lapply(imp,function(x) {
     data.table(
       pag=1:length(x) %>% rep(sapply(x,length))
       ,txt=x %>% unlist
     )})
+  pb <- progress_bar$new(format = "  [:bar] :elapsed :eta",total = length(imp), clear = FALSE, width= 60)
+  pb$tick(0)
   for(i in 1:length(imp)) {
     imp[[i]][,es:=grepl('[.!?\'\",]$',txt)]
     imp[[i]][,bp:=grepl(' {3,6}',txt)]
@@ -549,17 +585,15 @@ jpdf2imp<-function(jpdf,depth=5){
     imp[[i]]<-imp[[i]][,.(pag=range(pag) %>% unique %>% paste(collapse='-'),txt=txt %>% paste0(collapse=' ') %>% gsub('- ','',.) %>% gsub(' +',' ',.)),by=par]
     imp[[i]][,doc:=jpdf[i]]
     setcolorder(imp[[i]],c('doc','pag','par','txt'))
+    pb$tick()
   }
+  imp<-rbindlist(imp) %>% setkey(doc)
+  setkey(meta,doc)
   list(imp=imp,meta=meta)
 }
 
 jpdf<-jpdf2imp(dir('1895',full.names = T
                    #,pattern = '(Hirsch)|(Jenks)'
-                   ))
+))
+save(jpdf,file='fpdf.RData')
 #    getOption('viewer')(jpdf %>% paste0('#page=3'))
-
-if(F){
-  res<-microbenchmark(edge_betweenness=cluster_edge_betweenness(y),fast_greedy=cluster_fast_greedy(y),infomap=cluster_infomap(y),label_prop=cluster_label_prop(y),leading_eigen=cluster_leading_eigen(y),louvain=cluster_louvain(y),spinglass=cluster_spinglass(y),walktrap=cluster_walktrap(y))
-  ggplot2::autoplot(res)
-}
-
