beb466a14600ecb08e9852ef3eb5e0fefe1f45bb
Brooks Ambrose <brooksambrose@gmail.com>
2018-10-25 00:47:32 +0000

    ec update

--- tilit/R/ec.R
+++ tilit/R/ec.R
@@ -1,11 +1,12 @@
-#' Title
+#' Easy string concatenation
 #'
 #' @param x
+#' @param sp
 #'
 #' @return
 #' @export
 #'
 #' @examples
-ec<-function(x) {
-	strsplit(x,',')[[1]]
+ec<-function(x,sp=',') {
+	strsplit(x,sp)[[1]]
 }
