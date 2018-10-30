0aedba51317547eb0daeae3973e44ab9f68a7aa4
Brooks Ambrose <brooksambrose@gmail.com>
2018-10-30 05:31:13 +0000

    jclu: fix misnumbering of supers refine net plotting

--- plagiat/R/jclu2net.f.R
+++ plagiat/R/jclu2net.f.R
@@ -13,26 +13,30 @@
 #' @import igraph ggplot2 ggnetwork
 #'
 #' @examples
-jclu2net.f<-function(jclu,proj=c('gt','gd')[2],warn=T,layout='fruchtermanreingold',latent=F,isolates=F,...){
+jclu2net.f<-function(jclu,proj=c('gt','gd')[2],warn=T,layout='fruchtermanreingold',latent=F,isolates=T,...){
   if((V(jclu[[proj]]) %>% length)>100) if(warn) stop('Graphs over 100 nodes are slow and ugly.')
   m<-V(jclu[[proj]])$memb
   k<-!(table(m)-1)
   k<-names(k)[k]
-  net<-induced_subgraph(jclu[[proj]],which(!V(jclu[[proj]])$name%in%ifelse(isolates,unique(m) %>% setdiff(k),unique(m))),impl="create_from_scratch")
+  m %<>% unique
+  net<-induced_subgraph(
+    jclu[[proj]]
+    ,which(!V(jclu[[proj]])$name%in%(if(isolates) m %>% setdiff(k) else m ))
+    )
   net<-asNetwork(net)
   if(latent){
-      lnet<-pbapply::pblapply(1:6,function(x) ergmm(
-        net~euclidean(d=2,G=x)+
-          #nodemix('memb')+
-          edges
-        ,response='weight'
-        ,family='Poisson.log'
-        ,verbose=2
-        ,control=control.ergmm(
-          sample.size=1e4
-          ,burnin=1e6
-          ,interval=1e2
-        ))
+    lnet<-pbapply::pblapply(1:6,function(x) ergmm(
+      net~euclidean(d=2,G=x)+
+        #nodemix('memb')+
+        edges
+      ,response='weight'
+      ,family='Poisson.log'
+      ,verbose=2
+      ,control=control.ergmm(
+        sample.size=1e4
+        ,burnin=1e6
+        ,interval=1e2
+      ))
       ,cl = 6)
   }
   pd <- ggnetwork(
@@ -40,9 +44,9 @@ jclu2net.f<-function(jclu,proj=c('gt','gd')[2],warn=T,layout='fruchtermanreingol
     ,layout=layout,...)
   pd$memb<-factor(pd$memb,levels=levels(jclu[[sub('g','f',proj)]]))
   p <- ggplot(pd, aes(x = x, y = y, xend = xend, yend = yend,text=vertex.names)) +
-    geom_edges(aes(alpha=cross),size=.1) +
-    geom_nodes(aes(color=memb)) +
-    theme_blank() +
+    geom_edges(aes(alpha=cross,size=weight)) + scale_alpha_ordinal(range=c(.1,.5)) + scale_size_continuous(range=c(.1,2)) +
+    geom_nodes(aes(color=memb),size=3) +
+    theme_blank() + scale_color_brewer(type = 'qual',palette='Dark2') +
     theme(legend.title = element_blank())
   p
 }
@@ -52,18 +56,18 @@ jclu2net.f<-function(jclu,proj=c('gt','gd')[2],warn=T,layout='fruchtermanreingol
 # pd$n$memb<-factor(pd$n$memb,levels=levels(jclu[[sub('g','f',proj)]]))
 
 if(F){
-lnet<-pbapply::pblapply(1:6,function(x) ergmm(
-net~euclidean(d=2,G=x)+
-#nodemix('memb')+
-edges
-,response='weight'
-,family='Poisson.log'
-,verbose=2
-,control=control.ergmm(
-sample.size=1e4
-,burnin=1e6
-,interval=1e2
-))
-,cl = 6)
+  lnet<-pbapply::pblapply(1:6,function(x) ergmm(
+    net~euclidean(d=2,G=x)+
+      #nodemix('memb')+
+      edges
+    ,response='weight'
+    ,family='Poisson.log'
+    ,verbose=2
+    ,control=control.ergmm(
+      sample.size=1e4
+      ,burnin=1e6
+      ,interval=1e2
+    ))
+    ,cl = 6)
 
 }
--- plagiat/R/jstorm2jclu.f.R
+++ plagiat/R/jstorm2jclu.f.R
@@ -9,6 +9,8 @@
 #'
 #' @examples
 jstorm2jclu.f<-function(jstorm){
+
+  jstorm<-jstorm[title_id==title_history][sapply(discipline,length)>0]
   el<-jstorm[,.(discipline=unlist(discipline) %>% unique),by=title_history] %>% as.matrix
   g<-graph_from_edgelist(el)
   V(g)$type<-V(g)$name %in% el[,2]
@@ -19,34 +21,41 @@ jstorm2jclu.f<-function(jstorm){
 
 
   # label membership for disciplines with super
-  k<-data.table(d=igraph::degree(g$proj2),m=membership(cw2),name=V(g$proj2)$name)[,.(super=name[which.max(d)]),by=m] %>% setorder(m)
-  f2<-factor(k$super[membership(jclu$discipline)],levels = k$super)
+  k<-sapply(cw2 %>% communities,function(x) {
+    if(length(x)==1) return(x)
+    igraph::degree(induced_subgraph(g$proj2,V(g$proj2)$name%in%x)) %>% sort(decreasing=T) %>% head(1) %>% names
+  })
+  f2<-factor(k[membership(jclu$discipline)],levels = k[order(sapply(cw2 %>% communities,length),decreasing=T)])
   V(jclu$gd)$memb<-as.character(f2)
 
   jstormr<-jstorm[,.(discipline=discipline %>% unlist %>% unique),keyby=title_history]
   jstormr<-jstormr[data.table(th=jclu$title_history$names,m=jclu$title_history %>% membership) %>% setkey(th)]
   jstormr<-jstormr[,.N,by=.(m,discipline)] %>% setorder(m,-N)
   j<-jstormr[,.(super=discipline[1],N=sum(N)),by=m][,!'m']
-  f1<-factor(j$super[membership(jclu$title_history)],levels=j$super[order(j$N,decreasing = T)])
+  f1<-factor(j$super[membership(jclu$title_history)])
+  f1<-factor(f1 %>% as.character,levels = table(f1) %>% sort(decreasing = T) %>% names)
   V(jclu$gt)$memb<-as.character(f1)
+
   w<-j[,which(duplicated(super)|duplicated(super,fromLast = T))]
   if(length(w)) {
     s<-jstormr[,.(super=discipline[1:2] %>% paste(collapse=' (') %>% paste0(')')),by=m][w,super]
     j[w,super:=s]
     }
   jstormr<-j
-
   j<-data.table(title_history=jclu$title_history$names,super=jstormr[jclu$title_history %>% membership,super]) %>% setkey(title_history)
-  jstormr %>% setorder(-N)
-  jstormr[,Pct:=round(prop.table(N)*100,1)]
-  jstormr<-rbindlist(
-    list(jstormr,data.table('Total',jstormr[,2:3][,lapply(.SD,sum)]))
+
+  tab <-table(f1) %>% data.frame %>% data.table %>% setnames(ec('super,N'))
+  tab[,Pct:=round(N/jstorm[,.N]*100,1)]
+  og<-jstorm[,sapply(tab$super,function(x) sapply(discipline,function(y) x%in%y) %>% sum)]
+  tab<-rbindlist(
+    list(tab,data.table('Total',jstorm[,.N],tab[,sum(Pct)]))
   )
+  tab[,Labeled:=c(og,sum(og))][,LPct:=round(Labeled/jstorm[,.N]*100,1)]
 
   # crossings
   E(jclu$gd)$cross<-rev(c("within","between"))[(!crossing(jclu$discipline,jclu$gd)) + 1]
   E(jclu$gt)$cross<-rev(c("within","between"))[(!crossing(jclu$title_history,jclu$gt)) + 1]
 
 
-  c(jclu,ft=list(f1),fd=list(f2),tab=list(jstormr),super=list(j))
+  c(jclu,ft=list(f1),fd=list(f2),tab=list(tab),super=list(j))
 }
