9ebe3b79bc0a44f8030e315b3fd51c5114079d10
Brooks Ambrose <brooksambrose@berkeley.edu>
2019-02-08 21:40:02 -0800

    backup tree function

---
+++ plagiat/R/tree2sum.f.R
@@ -0,0 +1,13 @@
+#' K-Clique Community Treemap 2 Summary Statistics
+#'
+#' @param kcc2tree
+#'
+#' @return
+#' @export
+#'
+#' @examples
+# tree2sum.f<-function(kcc2tree){
+#   mel<-copy(bel2mel[[type]]) %>% setnames(ec('from,to,weight,id')) %>% setkey(from,to)
+#   gph<-graph_from_data_frame(mel,directed=F)
+#   deg<-degree(gph)
+# }
