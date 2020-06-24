1ab143ee67f085a6488e92c4bcbab71e2e266347
Brooks Ambrose <brooksambrose@berkeley.edu>
2020-06-23 20:29:15 -0700

    bump

--- tilit/R/nn.R
+++ tilit/R/nn.R
@@ -1,22 +1,24 @@
 #' Nice Number Format
 #'
+#' @param rnd
+#' @param cap
+#' @param word
 #' @param x
-#' @param ns
 #'
 #' @return
 #' @export
 #'
 #' @examples
-nn<-function(x,rnd=0,cap=F) {
+nn<-function(x,rnd=0,cap=F,word=T) {
   if(is.character(x)) return(x)
   x<-round(x,rnd)
   r<-rep(NA_character_,length(x))
   for(i in 1:length(x)){
     if(x[i]%%1==0&(x[i]+1)) {
-      r[i]<-c('zero','one','two','three','four','five','six','seven','eight','nine')[x[i]+1]
+      if(word) r[i]<-c('zero','one','two','three','four','five','six','seven','eight','nine')[x[i]+1]
       if(cap&i==1) r[i]<-paste(toupper(substring(r[i], 1,1)), substring(r[i], 2),sep="", collapse="")
     }
-    if(is.na(r[i])) r[i]<-format(x[i],big.mark=',',big.interval=3)
+    if(is.na(r[i])) r[i]<-format(x[i],big.mark=',',big.interval=3,nsmall=rnd)
   }
   r
 }
--- tilit/R/sg.R
+++ tilit/R/sg.R
@@ -12,24 +12,33 @@
 #' @param dig
 #' @param tabularx given preamble use new.col.align X for left Y for right Z for center
 #' @param untable remove table environment wrapping for document classes that don't support, e.g. moderncv
+#' @param nice
 #'
 #' @return
 #' @export
 #' @import data.table
 #' @importFrom magrittr %>%
+#' @importFrom lubridate is.timepoint
 #' @examples
-sg <- function(x,tit,dig=4,type,lab,sum=F,old.col.align=paste0(rep('c',ncol(x)),collapse=''),new.col.align=paste0(c('l',rep('r',ncol(x)-1)),collapse=''),rplc=c(old='^X\\. ',new=''),tabularx=F,untable=F,...) {
-  if(!is.data.table(x)) x<-data.table(x)
+sg <- function(x,tit,dig=4,nice=T,type,lab,sum=F,old.col.align=paste0(rep('c',ncol(x)),collapse=''),new.col.align=paste0(c('l',rep('r',ncol(x)-1)),collapse=''),rplc=c(old='^X\\. ',new=''),tabularx=F,untable=F,...) {
+  if(!is.data.table(x)) x<-data.table(x) else x<-copy(x)
   n<-names(x)[sapply(x,function(y) (!is.integer(y))&&is.numeric(y))]
   if(length(n)) x[,(n):=lapply(.SD,round,dig),.SDcols=n]
   n<-names(x)[sapply(x,is.factor)]
-  if(length(n)) x[,(n):=sapply(.SD,as.character),.SDcols=n]
+  if(length(n)) x[,(n):=lapply(.SD,as.character),.SDcols=n]
   if(missing(type)) type<-ifelse(knitr::is_latex_output(),'latex','html')
   if(missing(lab)) lab<-knitr::opts_current$get('label')
   if(is.null(lab)) lab<-''
   n<-names(x)[sapply(x,function(y) is.character(y)||is.factor(y))]
   if(!sum) {
     cn<-names(x)
+    if(nice) {
+      wn<-cn[sapply(x,is.numeric)]
+      nd<-x[,..wn] %>% lapply(function(y) strsplit(as.character(y),split='.',fixed=T) %>%
+        sapply(function(z) if(length(z)-1) nchar(z[2]) else 0) %>% max)
+      tl<-x[,..wn] %>% lapply(lubridate::is.timepoint)
+      for(i in names(nd)) if(tl[[i]]) x[,(i):=as.character((i))] else x[,(i):=format(get(i),digits=dig,big.mark = ',',big.interval = 3,nsmall = min(dig,nd[[i]]))]
+    }
     x<-data.frame(lapply(x,as.character),stringsAsFactors = F) %>% data.table %>% setnames(cn)
   }
   if(type=='html') {
@@ -40,8 +49,9 @@ sg <- function(x,tit,dig=4,type,lab,sum=F,old.col.align=paste0(rep('c',ncol(x)),
         x[,(n):=lapply(.SD,gsub,pattern='&',replacement='\t\b&',fixed=T),.SDcols=n]
       }
     }
-    x<-capture.output(stargazer::stargazer(x,type = 'html',title = tit,label=lab,style='ajs',summary=sum,header=F,rownames=F,align=ifelse(sum,T,F),...))
-  } else {
+    x<-capture.output(stargazer::stargazer(x,type = 'html',title = tit,label=lab,style='ajs',summary=sum,header=F,rownames=F,align=ifelse(sum,T,F),...)) %>%
+    {.[2]<-sub('text-align:center','text-align:right',.[2]);.} %>% {.[4] %<>% sub('tr><tr><td','tr><tbody><tr><td',.);.} %>% sub('</table>','</tbody></table>',.) %>% paste(collapse = '\n')
+    } else {
     setnames(x,esc.ltx(names(x)))
     if(!sum) if(length(n)) x[,(n):=lapply(.SD,esc.ltx),.SDcols=n]
     x<-capture.output(stargazer::stargazer(x,title = tit %>% esc.ltx,label=lab,style='ajs',summary=sum,header=F,rownames=F,align=ifelse(sum,T,F),...)) %>%
