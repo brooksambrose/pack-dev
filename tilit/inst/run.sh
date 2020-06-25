3b9a0e0a3e0d1df450a2de6cae20bbe6f742f98e
Brooks Ambrose <brooksambrose@berkeley.edu>
2020-06-24 21:58:14 -0700

    fixed detection of cpumemlog

--- tilit/inst/run.sh
+++ tilit/inst/run.sh
@@ -2,7 +2,5 @@
 function pgrep_live {
   pids=$(pgrep "$1");
   [ "$pids" ] || return;
-  ps -o s= -o pid= "$pids" | sed -n 's/^[^ZT][[:space:]]\+//p';
-}
-
-pgrep_live cpumemlog | if [ $? -eq 0 ] ; then $(Rscript -e 'cat(system.file("cpumemlog",package="tilit",mustWork=T))') 1 & sleep 5 ; fi
+  ps -o s= -o pid= ${pids} | sed -n 's/^[^ZT][[:space:]]\+//p';
+} && if [ ! $(pgrep_live cpumemlog) ] ; then $(Rscript -e 'cat(system.file("cpumemlog",package="tilit",mustWork=T))') 1 ; fi
