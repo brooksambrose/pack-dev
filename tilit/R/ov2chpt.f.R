cf54bb170b8e32763664720d5495aac5f5ec2a16
Brooks Ambrose <brooksambrose@gmail.com>
2021-10-11 20:47:54 -0700

    breaking changes; will now use all derivatives supplied for simultaneous analysis

--- tilit/R/ov2chpt.f.R
+++ tilit/R/ov2chpt.f.R
@@ -1,11 +1,11 @@
 #' Ordered Vector 2 Changepoints (now accepting matrix)
 #'
 #' @param x
-#' @param drv
+#' @param drv list all derivatives you want to calculate; multiples will be analyzed simultaneously.
 #' @param min.period
 #' @param warn
-#' @param inc.ov
-#' @param drv.only
+#' @param inc.ov include original variable
+#' @param drv.only derivative only, no grouping
 #' @param ... other args to ecp::e.divisive
 #'
 #' @return
@@ -18,11 +18,16 @@ ov2chpt.f<-function(x,drv=1,min.period=2,inc.ov=F,warn=T,drv.only=F,...){
   m<-'\nComputing long vectors is inefficient, warn=F to disable warning.'
   x<-data.table(x) %>% {.[,.SD,.SDcols=sapply(.,is.numeric)]}
   if(warn) if(is.null(d)) {if(length(x)>1000) stop(m)} else {if(nrow(x)>1000) stop(m)}
-  dif<-function(y){for(i in 1:drv) y<-diff(y);y}
-  r<-data.table(x,apply(x,2,function(z) c(rep(NA,drv),dif(z))))
-  setnames(r,c(names(x),paste0(names(x),'_d',drv)))
+  dif<-function(y) {
+    lapply(drv,function(i) {
+      for(j in 1:i) y<-diff(y)
+      y<-data.table(c(rep(NA,i),y)) %>% setnames(paste0(names(x),'_d',i))
+      y
+    }
+    ) %>% do.call(data.table,.)}
+  r<-lapply(x,dif) %>% unname %>% {do.call(data.table,c(list(x),.))} # breaking change
   if(inc.ov) j<-1:ncol(r) else j<-2:ncol(r)
-  if(!drv.only) r[,g:=factor(c(rep(1,drv-1),{k<-ecp::e.divisive(as.matrix(r[-(1:drv),.SD,.SDcols=j]),min.size = min.period,...)$cluster;c(k,max(k))}))]
+  if(!drv.only) r[,g:=factor(c(rep(1,max(drv)-1),{k<-ecp::e.divisive(as.matrix(r[-(1:max(drv)),.SD,.SDcols=j]),min.size = min.period,...)$cluster;c(k,max(k))}))]
   r
 }
 # TODO add this to SO answer at ?
